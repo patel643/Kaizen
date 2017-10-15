98d074b8b3a8d73fc7d34f0c167397507289e3e2
Keirann <ravin047@umn.edu>
2017-10-14 19:22:16 -0500

    db details are printed to console on /index route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  req.db.collection('usernotecollection').find().toArray(function(err, results){
+  console.log(results);
   res.render('index', {
-    user: req.user
+    user: req.user,
+    data: results
   });
 });
-
+});
 module.exports = router;
